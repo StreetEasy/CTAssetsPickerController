9528cc514c5d80a64edbcf0693f9f68fbc5a7d31
Wei Zhang <veeamd@gmail.com>
2017-11-13 17:00:56 -0500

    new framework name CTAssetsPicker

---
+++ CTAssetsPicker/CTAssetsPicker.h
@@ -0,0 +1,52 @@
+//
+//  CTAssetsPicker.h
+//  CTAssetsPicker
+//
+//  Created by Wei Zhang on 11/13/17.
+//  Copyright © 2017 Clement T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CTAssetsPicker.
+FOUNDATION_EXPORT double CTAssetsPickerVersionNumber;
+
+//! Project version string for CTAssetsPicker.
+FOUNDATION_EXPORT const unsigned char CTAssetsPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTAssetsPicker/PublicHeader.h>
+
+#import <CTAssetsPicker/CTAssetCheckmark.h>
+#import <CTAssetsPicker/CTAssetCollectionViewCell.h>
+#import <CTAssetsPicker/CTAssetCollectionViewController.h>
+#import <CTAssetsPicker/CTAssetItemViewController.h>
+#import <CTAssetsPicker/CTAssetPlayButton.h>
+#import <CTAssetsPicker/CTAssetScrollView.h>
+#import <CTAssetsPicker/CTAssetSelectionButton.h>
+#import <CTAssetsPicker/CTAssetSelectionLabel.h>
+#import <CTAssetsPicker/CTAssetThumbnailOverlay.h>
+#import <CTAssetsPicker/CTAssetThumbnailStacks.h>
+#import <CTAssetsPicker/CTAssetThumbnailView.h>
+#import <CTAssetsPicker/CTAssetsGridSelectedView.h>
+#import <CTAssetsPicker/CTAssetsGridView.h>
+#import <CTAssetsPicker/CTAssetsGridViewCell.h>
+#import <CTAssetsPicker/CTAssetsGridViewController.h>
+#import <CTAssetsPicker/CTAssetsGridViewFooter.h>
+#import <CTAssetsPicker/CTAssetsGridViewLayout.h>
+#import <CTAssetsPicker/CTAssetsNavigationController.h>
+#import <CTAssetsPicker/CTAssetsPageView.h>
+#import <CTAssetsPicker/CTAssetsPageViewController.h>
+#import <CTAssetsPicker/CTAssetsPickerAccessDeniedView.h>
+#import <CTAssetsPicker/CTAssetsPickerController.h>
+#import <CTAssetsPicker/CTAssetsPickerDefines.h>
+#import <CTAssetsPicker/CTAssetsPickerNoAssetsView.h>
+#import <CTAssetsPicker/CTAssetsViewControllerTransition.h>
+#import <CTAssetsPicker/NSBundle+CTAssetsPickerController.h>
+#import <CTAssetsPicker/NSDateFormatter+CTAssetsPickerController.h>
+#import <CTAssetsPicker/NSIndexSet+CTAssetsPickerController.h>
+#import <CTAssetsPicker/NSNumberFormatter+CTAssetsPickerController.h>
+#import <CTAssetsPicker/PHAsset+CTAssetsPickerController.h>
+#import <CTAssetsPicker/PHAssetCollection+CTAssetsPickerController.h>
+#import <CTAssetsPicker/PHImageManager+CTAssetsPickerController.h>
+#import <CTAssetsPicker/UICollectionView+CTAssetsPickerController.h>
+#import <CTAssetsPicker/UIImage+CTAssetsPickerController.h>
